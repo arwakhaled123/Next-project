e73ba16f6b97a3f68c87fcbaea96b43387a2a62e
arwakhaled123 <143205205+arwakhaled123@users.noreply.github.com>
2026-04-15 00:51:36 +0200

    Delete tasks directory

--- tasks/src/components/HomeComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomeComponent = () => {
-  return (
-    <h2>Home Page</h2>
-  )
-}
-
-export default HomeComponent
--- tasks/src/components/PostsComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PostsComponent = () => {
-  return (
-    <h3>Posts page</h3>
-  )
-}
-
-export default PostsComponent
